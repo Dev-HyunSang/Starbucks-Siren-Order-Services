719ba435d8937e3a21d9665ceec64be77951bcdc
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-12-21 09:10:23 +0900

    Feat: Auth에 대해서 개발함. 현재 사용자의 UUID를 불러오지 못함.

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -24,9 +25,10 @@ func createJWTToken(userUUID uuid.UUID) (*TokenDetails, error) {
 	td.AccessUUID = uuid.NewV4().String()
 
 	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
-	td.RefreshUUID = uuid.NewV4().String()
+	td.RefreshUUID = td.AccessUUID + "++" + userUUID.String()
 
 	var err error
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
 	Claims := jwt.MapClaims{}
 	Claims["authorized"] = true
 	Claims["access_uuid"] = td.AccessUUID
@@ -36,7 +38,9 @@ func createJWTToken(userUUID uuid.UUID) (*TokenDetails, error) {
 	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
 	if err != nil {
 		log.Fatalln("[ERROR] Failed to Create SignedString")
+		return nil, err
 	}
+
 	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf")
 	rtClaims := jwt.MapClaims{}
 	rtClaims["refresh_uuid"] = td.RefreshUUID
@@ -68,9 +72,9 @@ func CreateAuth(userUUID uuid.UUID, td *TokenDetails) error {
 }
 
 func ExtractToken(c *fiber.Ctx) string {
-	bearToken := c.Request().Header.Header()
+	bearToken := c.GetRespHeader("Authorization")
 	log.Println(bearToken)
-	strArr := strings.Split(string(bearToken), " ")
+	strArr := strings.Split(bearToken, " ")
 	if len(strArr) == 2 {
 		return strArr[1]
 	}
@@ -105,8 +109,8 @@ func TokenVailed(c *fiber.Ctx) error {
 	return nil
 }
 
-func ExtractTokenMetadata(r *fiber.Ctx) (*AccessDetails, error) {
-	token, err := VerifyToken(r)
+func ExtractTokenMetadata(c *fiber.Ctx) (*AccessDetails, error) {
+	token, err := VerifyToken(c)
 	if err != nil {
 		return nil, err
 	}
@@ -133,4 +137,128 @@ func FetchAuth(autD *AccessDetails) (string, error) {
 		return "Failed to Fetch Auth at Redis Server", err
 	}
 	return userUUID, nil
+}
+
+func DeleteAuth(givenUUID string) (int64, error) {
+	client := database.Redis()
+	deleted, err := client.Del(givenUUID).Result()
+	if err != nil {
+		log.Fatalln("Fail to DeleteAuth at Redis Delete")
+		return 0, err
+	}
+	return deleted, nil
+}
+
+func LogOut(c *fiber.Ctx) error {
+	au, err := ExtractTokenMetadata(c)
+	if err != nil {
+		log.Fatal("Failed to ExtractTokenMetadata")
+	}
+	deleted, delError := DeleteAuth(au.AccessUUID)
+	if delError != nil || deleted == 0 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"data": "unauthorized",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Successfully logged out",
+	})
+}
+
+func Refresh(c *fiber.Ctx) {
+	mapToken := map[string]string{}
+	if err := c.BodyParser(&mapToken); err != nil {
+		log.Fatalln("Failed to Refresh JSON Body Parser")
+		return
+	}
+	refreshToken := mapToken["refresh_token"]
+
+	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf")
+	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("REFRESH_SECRET")), nil
+	})
+	if err != nil {
+		fmt.Println("the error: ", err)
+		fiber.NewError(fiber.StatusUnauthorized, "Refresh token expired")
+		return
+	}
+
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status": fiber.StatusUnauthorized,
+			"data":   err,
+		})
+		return
+	}
+	clamis, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		refreshUUID, ok := clamis["refresh_uuid"].(string)
+		if !ok {
+			c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+				"status": fiber.StatusUnprocessableEntity,
+				"data":   err,
+			})
+			return
+		}
+
+		deleted, delErr := DeleteAuth(refreshUUID)
+		if delErr != nil || deleted == 0 {
+			c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"status": fiber.StatusUnauthorized,
+				"data":   "Unauthorized",
+			})
+			return
+		}
+		ts, createErr := createJWTToken(uuid.NewV4())
+		if createErr != nil {
+			c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"status": fiber.StatusForbidden,
+				"data":   createErr.Error(),
+			})
+			return
+		}
+		saveErr := CreateAuth(uuid.NewV4(), ts)
+		if saveErr != nil {
+			c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"status": fiber.StatusForbidden,
+				"data":   saveErr.Error(),
+			})
+			return
+		}
+		tokens := map[string]string{
+			"access_token":  ts.AccessToken,
+			"refresh_token": ts.RefreshToken,
+		}
+		c.Status(fiber.StatusCreated).JSON(fiber.Map{
+			"status": fiber.StatusCreated,
+			"data":   tokens,
+		})
+		return
+	} else {
+		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status": fiber.StatusUnauthorized,
+			"data":   "refresh expired",
+		})
+		return
+	}
+}
+
+func DeleteTokens(authD *AccessDetails) error {
+	refreshUUID := fmt.Sprintf("%s++%d", authD.AccessUUID, authD.UserUUID)
+	client := database.Redis()
+	deleteAt, err := client.Del(authD.AccessUUID).Result()
+	if err != nil {
+		return err
+	}
+	deleteRt, err := client.Del(refreshUUID).Result()
+	if err != nil {
+		return err
+	}
+	if deleteAt != 1 || deleteRt != 1 {
+		return errors.New("something went wrong")
+	}
+	return nil
 }
