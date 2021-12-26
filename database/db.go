e131622b864af7918849eb7365fbc002e6bb5aea
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-12-26 19:19:28 +0900

    Feat: 데이터베이스 정보 변경

--- database/db.go
+++ database/db.go
@@ -3,17 +3,13 @@ package database
 import (
 	"fmt"
 
-	"github.com/dev-hyunsang/siren-order/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectionDataBase() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", "root", "parkhs0625", "127.0.0.1:3306", "siren_order")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", "root", "parkhs0625", "127.0.0.1:3306", "starbucks_siren_order")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// 구조체를 이용한 테이블 자동 생성
-	db.AutoMigrate(&model.Users{})
 
 	return db, err
 }
