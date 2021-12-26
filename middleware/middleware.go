7728a6be5f47a166b310bd2129e555ec7acc68f2
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-12-26 19:20:01 +0900

    Feat: 구조체 타입 변경 및 미들웨이 변경

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,9 +3,20 @@ package middleware
 import (
 	"github.com/dev-hyunsang/siren-order/cmd"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Route(app *fiber.App) {
-	member := app.Group("/member")
-	member.Post("/register", cmd.CreateUser)
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	member := app.Group("/api/member")
+	member.Post("/register", cmd.Register)
+	member.Post("/login", cmd.Login)
+	member.Get("/auth", cmd.Auth)
+	member.Post("/logout", cmd.Logout)
+
+	home := app.Group("/api/home")
+	home.Get("/comment", cmd.Comment)
 }
--- model/users.go
+++ model/users.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Users struct {
-	UUID      uuid.UUID  `json:"uuid"`
-	Name      string     `json:"name"`
-	NickName  string     `json:"nickname"`
-	Birthday  *time.Time `json:"birthday"`
-	Email     string     `json:"email"`
-	Password  string     `json:"password"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
+	UUID      uuid.UUID `json:"uuid"`
+	Name      string    `json:"name"`
+	NickName  string    `json:"nickname"`
+	Birthday  string    `json:"birthday"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
