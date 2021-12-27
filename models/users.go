e501eaf3001eb91e50354c3a65e9fd310e404183
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-12-27 21:14:03 +0900

    Feat: 디렉토리 이름 변경

---
+++ models/users.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/twinj/uuid"
+)
+
+type Users struct {
+	UUID      uuid.UUID `json:"uuid"`
+	Name      string    `json:"name"`
+	NickName  string    `json:"nickname"`
+	Birthday  string    `json:"birthday"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
